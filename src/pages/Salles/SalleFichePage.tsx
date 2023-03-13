f0671a4bb623831e1aefd7015b1b529b86a4e2ae
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-13 17:09:34 +0100

    création page SalleFichePage qui retourne SalleList compoenent + SalleFiche compoenent

--- src/pages/Salles/SalleFichePage.tsx
+++ src/pages/Salles/SalleFichePage.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SalleFiche from '../../components/fiche/SalleFiche'
+import SalleList from '../../components/lists/SalleList'
+import { Salle } from '../../models/Salle'
+import { salleService } from '../../services/SalleService'
 
 const SalleFichePage = () => {
+
+  const [salles, setSalles] = useState<Salle[]>([])
+
+  const [currentPage, setCurrentPage] = useState('Fiche')
+
+  useEffect(() => {
+    getAllSalles()
+  }, [])
+
+  const getAllSalles = () => {
+    salleService.findAllSalles().then(data => setSalles(data))
+  }
+
+  const handleUpdateSalle = (salle : Salle) => {
+    setSalles(salles.map((s) => (s.id === salle.id ? salle : s)));
+  }
+
   return (
-    <div>SalleFichePage</div>
+    <>
+      {salles &&
+        <>
+          <SalleFiche salles={salles} onUpdateSalle={handleUpdateSalle}/>
+          <SalleList salles={salles} currentPage={currentPage}/>
+        </>
+      }
+    </>
   )
 }
 
