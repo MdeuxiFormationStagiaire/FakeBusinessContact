56f244baec484708496ecf4334e75f563bf65500
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-13 15:38:54 +0100

    création de la page Salles

--- src/pages/Salles/SallesPage.tsx
+++ src/pages/Salles/SallesPage.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SalleList from '../../components/lists/SalleList';
+import { Salle } from '../../models/Salle';
+import { salleService } from '../../services/SalleService';
 
 const SallesPage = () => {
+
+  const [salles, setSalles] = useState<Salle[]>([]);
+
+  const [currentPage, setCurrentPage] = useState('')
+
+  useEffect(() => {
+    getAllSalles()
+  }, [])
+
+  const getAllSalles = () => {
+    salleService.findAllSalles().then(data => setSalles(data))
+  }
+  
   return (
-    <div>SallesPage</div>
+    <>
+      <a href="/stagiaires/add" className='addButtonBox'>
+        <button className='addButton'>Ajouter</button>
+      </a>
+      <SalleList salles={salles} currentPage={currentPage}/>
+    </>
   )
 }
 
