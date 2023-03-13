aaacf98a9ee72a87a13c89c7c0e84a4f070a4e01
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-13 19:15:40 +0100

    add undefined type to floor prop

--- src/models/Salle.ts
+++ src/models/Salle.ts
@@ -2,7 +2,7 @@ export type Salle = {
     id: number,
     capacity: number,
     name: string,
-    floor: "RDC" | "1er" | "2ème",
+    floor: "RDC" | "1er" | "2ème" | undefined,
     indication: string,
     createdAt : Date
 }
