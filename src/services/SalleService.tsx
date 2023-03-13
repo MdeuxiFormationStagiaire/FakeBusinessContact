413f1ca9ecd9c5dd49fb34cf58b134246f0ca5fb
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-13 15:26:37 +0100

    salleService done

--- src/services/SalleService.tsx
+++ src/services/SalleService.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Salle } from "../models/Salle"
 
-const SalleService = () => {
-  return (
-    <div>SalleService</div>
-  )
+const URL : string = "http://localhost:3000/salles"
+
+class SalleService {
+
+    findAllSalles = async () => {
+        const res = await fetch(URL);
+        return await res.json();
+    }
+
+    getSalleById = async (id : number) => {
+        const res = await fetch(`${URL}/${id}`, {
+            method: "GET",
+        });
+        return await res.json();
+    }
+
+    createSalle = async (salle : Salle) => {
+        const res = await fetch(URL, {
+            method: "POST",
+            body: JSON.stringify(salle),
+            headers: { "content-type": "application/json" }
+        });
+        return await res.json();
+    }
+
+    deleteSalle = async (id : number) => {
+        const res = await fetch(`${URL}/${id}`, {
+            method: "DELETE",
+        });
+        return await res.json();
+    }
+
+    updateSalle = async (id : number, salle : Salle) => {
+        const res = await fetch(`${URL}/${id}`, {
+            method: "PATCH",
+            body: JSON.stringify(salle),
+            headers: { "content-type": "application/json" }
+        });
+        return await res.json();
+    }
+
 }
 
-export default SalleService
+export const salleService = Object.freeze(new SalleService)
