ddc589b9c8fc99172e0dcec85c1eca9dd9c9ad9f
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-13 18:06:43 +0100

    maj de la prop floor dans le type Salle.ts

--- src/models/Salle.ts
+++ src/models/Salle.ts
@@ -2,7 +2,7 @@ export type Salle = {
     id: number,
     capacity: number,
     name: string,
-    floor: string,
+    floor: "RDC" | "1er" | "2ème",
     indication: string,
     createdAt : Date
 }
