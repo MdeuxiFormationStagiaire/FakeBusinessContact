684520a893aa629de739bba3e9960eb62c2a9a89
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-13 18:54:11 +0100

    setup SalleAddPage with AddSalle component + SalleList component

--- src/pages/Salles/SalleAddPage.tsx
+++ src/pages/Salles/SalleAddPage.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AddSalle from '../../components/add/AddSalle'
+import SalleList from '../../components/lists/SalleList'
+import { Salle } from '../../models/Salle'
+import { salleService } from '../../services/SalleService'
 
 const SalleAddPage = () => {
+
+  const [salles, setSalles] = useState<Salle[]>([])
+
+  const [currentPage, setCurrentPage] = useState('Add')
+
+  useEffect(() => {
+    getAllSalles()
+  }, [])
+
+  const getAllSalles = () => {
+    salleService.findAllSalles().then(data => setSalles(data))
+  }
+  
+  const addNewSalle = (salle : Salle) => {
+    salleService.createSalle(salle).then(() => getAllSalles())
+  }
+
+  
   return (
-    <div>SalleAddPage</div>
+    <>
+      <AddSalle addNewSalle={addNewSalle}/>
+      <SalleList salles={salles} currentPage={currentPage}/>
+    </>
   )
 }
 
