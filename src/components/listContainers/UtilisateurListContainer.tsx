a6ec64eb5397405c298d9e06bf0680b428057fba
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-14 11:42:42 +0100

    update UtilisateurListContainer for matching Utilisateur props

--- src/components/listContainers/UtilisateurListContainer.tsx
+++ src/components/listContainers/UtilisateurListContainer.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { Utilisateur } from '../../models/Utilisateur'
 
-const UtilisateurListContainer = () => {
+type UtilisateurListContainerProps = {
+    utilisateur: Utilisateur,
+    onUtilisateurSelected: (utilisateur : Utilisateur) => void;
+}
+
+const UtilisateurListContainer : React.FC<UtilisateurListContainerProps> = ({utilisateur, onUtilisateurSelected}) => {
+  
+  const [isSelected, setIsSelected] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleUtilisateurSelected = () => {
+    setIsSelected(true);
+    onUtilisateurSelected(utilisateur);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        setIsSelected(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div>UtilisateurListContainer</div>
+    <Link to={`/utilisateurs/${utilisateur.id}`} className="ficheLink">
+    <div
+      className={`itemsContainer ${isSelected ? 'itemsContainerSelected' : ''}`}
+      onClick={handleUtilisateurSelected}
+      ref={containerRef}
+    >
+      <span className="items">{utilisateur.last_name}</span>
+      <span className="items">{utilisateur.first_name}</span>
+      <span className="items">{utilisateur.email}</span>
+      <span className="items">{utilisateur.position}</span>
+      <span className="items2">
+        {utilisateur.createdAt.toLocaleString('fr-FR').slice(8, 10) +
+          '/' +
+          utilisateur.createdAt.toLocaleString('fr-FR').slice(5, 7) +
+          '/' +
+          utilisateur.createdAt.toLocaleString('fr-FR').slice(0, 4)}
+      </span>
+    </div>
+    </Link>
   )
 }
 
