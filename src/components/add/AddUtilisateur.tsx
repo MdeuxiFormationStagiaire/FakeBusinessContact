4a50afac7f5c6357896af6a9eb30be4a4789716a
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-15 14:55:25 +0100

    set AddUtilisateur component + css

---
+++ src/components/add/AddUtilisateur.tsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Utilisateur } from '../../models/Utilisateur';
+import '../../assets/styles/components/add/AddUtilisateur.css'
+
+type AddUtilisateurProps = {
+  addNewUtilisateur : Function
+}
+
+const AddUtilisateur : React.FC<AddUtilisateurProps> = ({addNewUtilisateur}) => {
+
+  const navigate = useNavigate();
+  const [utilisateur, setUtilisateur] = useState<Utilisateur>({ id: 0, first_name: '', last_name: '', email: '', position: '', createdAt: new Date(), adminRight: false });
+
+  useEffect(() => {
+    setUtilisateur((utilisateur) => ({...utilisateur, createdAt: new Date()}));
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, checked } = event.target;
+    if (name === 'adminRight') {
+      setUtilisateur((utilisateur) => ({...utilisateur, [name]: checked}));
+    } else {
+      setUtilisateur((utilisateur) => ({...utilisateur, [name]: value}));
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const utilisateurCapitalized : Utilisateur = {
+      ...utilisateur,
+      first_name: utilisateur.first_name.charAt(0).toUpperCase() + utilisateur.first_name.toLocaleLowerCase().slice(1),
+      last_name: utilisateur.last_name.charAt(0).toUpperCase() + utilisateur.last_name.toLocaleLowerCase().slice(1),
+      email: utilisateur.email.toLocaleLowerCase(),
+      position: utilisateur.position.charAt(0).toUpperCase() + utilisateur.position.toLocaleLowerCase().slice(1)
+    };
+
+    const response = await fetch('http://localhost:3000/utilisateurs');
+    const data = await response.json();
+    const duplicateUtilisateur = data.find((u : Utilisateur) => u.email === utilisateurCapitalized.email);
+
+    if (duplicateUtilisateur) {
+      alert('Ce stagiaire existe déjà !');
+    } else {
+      addNewUtilisateur(utilisateurCapitalized);
+      const newUtilisateur : Utilisateur = { id: 0, first_name: '', last_name: '', email: '', position: '', createdAt: new Date(), adminRight: false };
+      setUtilisateur(newUtilisateur);
+    };
+  };
+
+  return (
+    <>
+      <form className='addFormSectionUtilisateurs' onSubmit={(event) => handleSubmit(event)}>
+        <div className="buttonDivBoxUtilisateurs">
+          <button type="submit" className='addFormButtonUtilisateurs'>Ajouter</button>
+          <button type='button' className='cancelButtonUtilisateurs' onClick={() => navigate(-1)}>Annuler</button>
+        </div>
+        <section className="addFicheSectionUtilisateurs">
+          <div className="inputDivBoxUtilisateurs">
+            <div className='inputDivUtilisateurs'>
+              <label htmlFor="first_name" className='addInputTitleUtilisateurs'>Nom :</label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                className='firstNameInputUtilisateurs'
+                value={utilisateur.first_name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className='inputDivUtilisateurs'>
+              <label htmlFor="last_name" className='addInputTitleUtilisateurs'>Prénom :</label>
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                className='lastNameInputUtilisateurs'
+                value={utilisateur.last_name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className='inputDivUtilisateurs'>
+              <label htmlFor="email" className='addInputTitleUtilisateurs'>Email :</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className='emailInputUtilisateurs'
+                value={utilisateur.email}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className='inputDivUtilisateurs'>
+              <label htmlFor="position" className='addInputTitleUtilisateurs'>Fonction :</label>
+              <input
+                type="text"
+                name="position"
+                id="position"
+                className='positionInputUtilisateurs'
+                value={utilisateur.position}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className='inputDivUtilisateurs'>
+              <label htmlFor="adminRight" className='addInputTitleUtilisateurs'>Admin :</label>
+              <input
+                type="checkbox"
+                name="adminRight"
+                id="adminRight"
+                className='adminRightInputUtilisateurs'
+                checked={utilisateur.adminRight}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </div>
+        </section>
+      </form>
+    </>
+  )
+}
+
+export default AddUtilisateur
