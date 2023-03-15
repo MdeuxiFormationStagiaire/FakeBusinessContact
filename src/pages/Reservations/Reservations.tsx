29930551774520c6433c165fb3e97b8e3c3a07f1
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-15 18:04:44 +0100

    setup ReservationsPage with AutreList component and PromotionList component

--- src/pages/Reservations/Reservations.tsx
+++ src/pages/Reservations/Reservations.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import AutreList from '../../components/lists/AutreList';
+import PromotionList from '../../components/lists/PromotionList';
+import { Autre } from '../../models/Reservation/Autre';
+import { Promotion } from '../../models/Reservation/Promotion';
+import { autreService } from '../../services/AutreService';
+import { promotionService } from '../../services/PromotionService';
 
 const Reservations = () => {
+  
+  const navigate = useNavigate();
+  
+  const [promotions, setPromotions] = useState<Promotion[]>([]);
+
+  const [autres, setAutres] = useState<Autre[]>([]);
+
+  useEffect(() => {
+    getAllPromotions();
+    getAllAutres();
+  }, [])
+
+  const getAllPromotions = () => {
+    promotionService.findAllPromotions().then(data => setPromotions(data))
+  }
+
+  const getAllAutres = () => {
+    autreService.findAllAutres().then(data => setAutres(data))
+  }
+
+  const handleAddButtonNav = () => {
+    navigate('/reservations/add')
+  };
+
   return (
-    <div className='title'>ReservationPage</div>
+    <>
+      <div className='addButtonBox' onClick={handleAddButtonNav}>
+        <button className='addButton'>Ajouter</button>
+      </div>
+      <PromotionList promotions={promotions}/>
+      <AutreList autres={autres}/>
+    </>
   )
 }
 
