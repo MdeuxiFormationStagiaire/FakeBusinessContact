ccceec6da4e036df2f29c3d1a437e63cadd64dd1
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-15 14:05:37 +0100

    set UtilisateurAddPage with UtilisateurList component and AddUtilisateur component

--- src/pages/Utilisateurs/UtilisateurAddPage.tsx
+++ src/pages/Utilisateurs/UtilisateurAddPage.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AddUtilisateur from '../../components/add/AddUtilisateur'
+import UtilisateurList from '../../components/lists/UtilisateurList'
+import { Utilisateur } from '../../models/Utilisateur'
+import { utilisateurService } from '../../services/UtilisateurService'
 
 const UtilisateurAddPage = () => {
+
+  const [utilisateurs, setUtilisateurs] = useState<Utilisateur[]>([])
+
+  const [currentPage, setCurrentPage] = useState('Add')
+
+  useEffect(() => {
+    getAllUtilisateurs()
+  }, [])
+
+  const getAllUtilisateurs = () => {
+    utilisateurService.findAllUtilisateurs().then(data => setUtilisateurs(data))
+  }
+  
+  const addNewUtilisateur = (utilisateur : Utilisateur) => {
+    utilisateurService.createUtilisateur(utilisateur).then(() => getAllUtilisateurs())
+  }
+  
   return (
-    <div>UtilisateurAddPage</div>
+    <>
+      <AddUtilisateur addNewUtilisateur={addNewUtilisateur}/>
+      <UtilisateurList utilisateurs={utilisateurs} currentPage={currentPage}/>
+    </>
   )
 }
 
