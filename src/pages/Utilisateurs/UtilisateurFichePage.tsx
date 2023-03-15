4d0d48eeedff71a429e14b07832f8185278a5d0f
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-15 11:47:58 +0100

    add UtilisateurFichePage with UtilisateurFiche component and UtilisateurList component

--- src/pages/Utilisateurs/UtilisateurFichePage.tsx
+++ src/pages/Utilisateurs/UtilisateurFichePage.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import UtilisateurFiche from '../../components/fiche/UtilisateurFiche'
+import UtilisateurList from '../../components/lists/UtilisateurList'
+import { Utilisateur } from '../../models/Utilisateur'
+import { utilisateurService } from '../../services/UtilisateurService'
 
 const UtilisateurFichePage = () => {
+
+  const [utilisateurs, setUtilisateurs] = useState<Utilisateur[]>([])
+
+  const [currentPage, setCurrentPage] = useState('Fiche')
+
+  useEffect(() => {
+    getAllStagiaire()
+  }, [])
+
+  const getAllStagiaire = () => {
+    utilisateurService.findAllUtilisateurs().then(data => setUtilisateurs(data))
+  }
+
+  const handleUpdateUtilisateur = (utilisateur : Utilisateur) => {
+    setUtilisateurs(utilisateurs.map((u) => (u.id === utilisateur.id ? utilisateur : u)));
+  }
+
   return (
-    <div>UtilisateurFichePage</div>
+    <>
+      {utilisateurs &&
+        <>
+          <UtilisateurFiche utilisateurs={utilisateurs} onUpdateUtilisateur={handleUpdateUtilisateur}/>
+          <UtilisateurList utilisateurs={utilisateurs} currentPage={currentPage}/>
+        </>
+      }
+    </>
   )
 }
 
