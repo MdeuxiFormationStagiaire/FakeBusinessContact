0116f47ab575b97def21997cf688be1989d5559f
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-21 14:17:19 +0100

    setup list and listContainer for promotions

---
+++ src/components/listContainers/PromotionListContainer.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { Promotion } from '../../models/Reservation/Promotion'
+import '../../assets/styles/components/listContainer/PromotionListContainer.css'
+
+type PromotionListContainerProsp = {
+    promotion: Promotion;
+    onPromotionSelected: (promotion: Promotion) => void;
+}
+
+const PromotionListContainer : React.FC<PromotionListContainerProsp> = ({promotion, onPromotionSelected}) => {
+  
+    const [isSelected, setIsSelected] = useState(false);
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handlePromotionSelected = () => {
+        setIsSelected(true);
+        onPromotionSelected(promotion);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+                setIsSelected(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    const formateDate = (date : Date) => {
+        const formatedDate : string = 
+            date.toLocaleString('fr-FR').slice(8, 10) + '/' +
+            date.toLocaleString('fr-FR').slice(5, 7) + '/' +
+            date.toLocaleString('fr-FR').slice(0, 4)
+        ;
+        return formatedDate;
+    }
+
+    return (
+        <Link to={`/promotions/${promotion.id}`} className="ficheLink">
+        <div
+          className={`itemsContainerPromotions ${isSelected ? 'itemsContainerSelectedPromotions' : ''}`}
+          onClick={handlePromotionSelected}
+          ref={containerRef}
+        >
+          <span className="itemsPromotions">{promotion.description}</span>
+          <span className="items2Promotions">{formateDate(promotion.createdAt)}</span>
+          <span className="items2Promotions">{formateDate(promotion.startAt)}</span>
+          <span className="items2Promotions">{formateDate(promotion.endAt)}</span>
+          <span className="itemsPromotions">{`${promotion.formateur.first_name} ${promotion.formateur.last_name}`}</span>
+          <span className="itemsPromotions">{promotion.stagiaires.length}</span>
+          <span className="itemsPromotions">{promotion.salle.name}</span>
+          <span className="itemsPromotions">{`${promotion.utilisateur.first_name} ${promotion.utilisateur.last_name}`}</span>
+        </div>
+      </Link>
+    )
+}
+
+export default PromotionListContainer
--- src/components/lists/PromotionList.tsx
+++ src/components/lists/PromotionList.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Promotion } from '../../models/Reservation/Promotion'
+import PromotionListContainer from '../listContainers/PromotionListContainer';
+import '../../assets/styles/components/lists/PromotionList.css'
 
 type PromotionListProps = {
     promotions: Promotion[];
@@ -7,8 +9,98 @@ type PromotionListProps = {
 }
 
 const PromotionList : React.FC<PromotionListProps> = ({promotions, currentPage}) => {
+  
+  const [filterOrder, setFilterOrder] = useState<string>('ascendant')
+  const [search, setSearch] = useState<string>('')
+  const [selectedPromotion, setSelectedPromotion] = useState<Promotion | null>(null);
+
+  const filterBySalle = () => {
+    promotions.sort((a, b) => {
+      const salleA : string = `${a.salle.name}`;
+      const salleB : string = `${b.salle.name}`;
+      if (filterOrder === 'ascendant') {
+        return salleA.localeCompare(salleB);
+      } else {
+        return salleB.localeCompare(salleB);
+      };
+    });
+    setFilterOrder(filterOrder === 'ascendant' ? 'descendant' : 'ascendant');
+  }
+
+  const filterByFormateur = () => {
+    promotions.sort((a, b) => {
+      const formateurA : string = `${a.formateur.first_name} ${a.formateur.last_name}`;
+      const formateurB : string = `${b.formateur.first_name} ${b.formateur.last_name}`;
+      if (filterOrder === 'ascendant') {
+        return formateurA.localeCompare(formateurB);
+      } else {
+        return formateurB.localeCompare(formateurA);
+      };
+    });
+    setFilterOrder(filterOrder === 'ascendant' ? 'descendant' : 'ascendant');
+  }
+
+  const handlePromotionSelected = (promotion: Promotion) => {
+    setSelectedPromotion(promotion);
+  };
+
+  const renderPromotionsList = () => {
+
+    const filteredPromotions = promotions.filter((promotion: Promotion) => {
+      const name = 
+      `
+        ${promotion.description.toLocaleLowerCase()} 
+        ${promotion.formateur.first_name.toLocaleLowerCase()} 
+        ${promotion.formateur.last_name.toLocaleLowerCase()} 
+        ${promotion.salle.name.toLocaleLowerCase()} 
+        ${promotion.utilisateur.first_name.toLocaleLowerCase()} 
+        ${promotion.utilisateur.last_name.toLocaleLowerCase()}
+      `;
+      return search === '' ? promotion : name.includes(search);
+    });
+
+    return filteredPromotions.map((promotion: Promotion) => {
+      return (
+        <PromotionListContainer
+          key={promotion.id} 
+          promotion={promotion} 
+          onPromotionSelected={handlePromotionSelected}/>
+      );
+    });
+
+  }
+  
   return (
-    <div>PromotionList</div>
+    <>
+    <section className={'listPromotions' + `${currentPage}`}>
+      <div className="filterBarpromotions">
+        <button className="filterAllButtonPromotions">{promotions.length} Promotions</button>
+        <button onClick={filterBySalle} className="filterNameButtonPromotions">
+          By Salle
+        </button>
+        <button onClick={filterByFormateur} className="filterNameButtonPromotions">
+          By Formateur
+        </button>
+        <input
+          className="searchInputPromotions"
+          type="search"
+          placeholder="   Recherche ..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="gridPromotions">
+        <h3 className="gridTitlePromotions">Description</h3>
+        <h3 className="gridTitlePromotions">DC</h3>
+        <h3 className="gridTitlePromotions">DD</h3>
+        <h3 className="gridTitlePromotions">DF</h3>
+        <h3 className="gridTitlePromotions">Formateur</h3>
+        <h3 className="gridTitlePromotions">Effectifs</h3>
+        <h3 className="gridTitlePromotions">Salle</h3>
+        <h3 className="gridTitlePromotions">Collaborateurs</h3>
+      </div>
+      <div className="promotionsListContainer">{renderPromotionsList()}</div>
+    </section>
+  </>
   )
 }
 
