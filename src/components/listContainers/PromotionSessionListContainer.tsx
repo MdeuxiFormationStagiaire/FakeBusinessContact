6aa42ae597b11b59303e02593ef83f135339d18d
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-22 16:28:34 +0100

    setup Sessions and Stagaires listContainers of selected Promotion

---
+++ src/components/listContainers/PromotionSessionListContainer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Session } from '../../models/Reservation/Session'
+
+type PromotionSessionListContainerProps = {
+    session: Session
+}
+
+const PromotionSessionListContainer : React.FC<PromotionSessionListContainerProps>= ({session}) => {
+
+    const formateDate = (date : Date) => {
+        const formatedDate : string = 
+            date.toLocaleString('fr-FR').slice(8, 10) + '/' +
+            date.toLocaleString('fr-FR').slice(5, 7) + '/' +
+            date.toLocaleString('fr-FR').slice(0, 4)
+        ;
+        return formatedDate;
+      }
+
+  return (
+<div>
+    <span className="itemsSessionsPromotion">{session.desc}</span>
+    <span className="itemsSessionsPromotion">{formateDate(session.startAt)}</span>
+    <span className="itemsSessionsPromotion">{formateDate(session.endAt)}</span>
+    <span className="itemsSessionsPromotion">{`${session.formateur.first_name} ${session.formateur.last_name}`}</span>
+</div>
+  )
+}
+
+export default PromotionSessionListContainer
---
+++ src/components/listContainers/PromotionStagiaireListContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Stagiaire } from '../../models/Stagiaire'
+
+type PromotionStagiaireListContainerProps = {
+    stagiaire: Stagiaire
+}
+
+const PromotionStagiaireListContainer : React.FC<PromotionStagiaireListContainerProps> = ({stagiaire}) => {
+  
+return (
+<div>
+    <span className="itemsStagiairesPromotion">{stagiaire.last_name}</span>
+    <span className="itemsStagiairesPromotion">{stagiaire.first_name}</span>
+    <span className="itemsStagiairesPromotion">{stagiaire.email}</span>
+</div>
+)
+}
+
+export default PromotionStagiaireListContainer
