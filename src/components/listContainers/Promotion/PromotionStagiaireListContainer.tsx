febdc618a0d100700271364664a1fa4a6487e104
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-24 18:35:14 +0100

    rework of component and make it simple

--- src/components/listContainers/Promotion/PromotionStagiaireListContainer.tsx
+++ src/components/listContainers/Promotion/PromotionStagiaireListContainer.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Stagiaire } from '../../../models/Stagiaire'
 import '../../../assets/styles/components/listContainer/PromotionFicheListContainer.css'
+import { promotionService } from '../../../services/Reservation/PromotionService'
+import { Promotion } from '../../../models/Reservation/Promotion'
 
 type PromotionStagiaireListContainerProps = {
+    promotion: Promotion
     stagiaire: Stagiaire
+    editMode: boolean
+    addMode: boolean
+    validate: boolean
+    onDeleteStagiaire: Function
 }
 
-const PromotionStagiaireListContainer : React.FC<PromotionStagiaireListContainerProps> = ({stagiaire}) => {
-  
-return (
-<div  className='itemsContainerStagiairesPromotion'>
-    <span className="itemsStagiairesPromotion">{stagiaire.last_name}</span>
-    <span className="itemsStagiairesPromotion">{stagiaire.first_name}</span>
-    <span className="items2StagiairesPromotion">{stagiaire.email}</span>
-</div>
-)
+const PromotionStagiaireListContainer : React.FC<PromotionStagiaireListContainerProps> = ({promotion, stagiaire, editMode, addMode, validate, onDeleteStagiaire}) => {
+    return (
+        <>
+            {editMode ? (
+                <div  className='itemsContainerStagiairesPromotion'>
+                    <span className="itemsStagiairesPromotion">{stagiaire.last_name}</span>
+                    <span className="itemsStagiairesPromotion">{stagiaire.first_name}</span>
+                    <span className="items2StagiairesPromotion">{stagiaire.email}</span>
+                    <button onClick={() => onDeleteStagiaire(stagiaire.id)}>X</button>
+                </div>
+            ) : (
+                <div  className='itemsContainerStagiairesPromotion'>
+                    <span className="itemsStagiairesPromotion">{stagiaire.last_name}</span>
+                    <span className="itemsStagiairesPromotion">{stagiaire.first_name}</span>
+                    <span className="items2StagiairesPromotion">{stagiaire.email}</span>
+                </div>
+            )}
+        </>
+    )
 }
 
 export default PromotionStagiaireListContainer
