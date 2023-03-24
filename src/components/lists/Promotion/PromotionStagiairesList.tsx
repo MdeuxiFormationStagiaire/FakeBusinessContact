3c764624345b00081615e4ff343f477fea1c33f0
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-24 18:36:18 +0100

    add handleDeleteStagiaire method and ternaire on editMode for diplsay

--- src/components/lists/Promotion/PromotionStagiairesList.tsx
+++ src/components/lists/Promotion/PromotionStagiairesList.tsx
@@ -2,39 +2,27 @@ import React, { useState } from 'react'
 import { Stagiaire } from '../../../models/Stagiaire'
 import PromotionStagiaireListContainer from '../../listContainers/Promotion/PromotionStagiaireListContainer'
 import updateLogo from '../../../assets/img/modify.png'
+import validateLogo from '../../../assets/img/checked.png'
+import addUserLogo from '../../../assets/img/ajouter-un-utilisateur.png'
 import '../../../assets/styles/components/lists/PromotionFicheList.css'
 import { Promotion } from '../../../models/Reservation/Promotion'
+import { promotionService } from '../../../services/Reservation/PromotionService'
 
 type PromotionStagiairesListProps = {
   promotion: Promotion
-  stagiaires: Stagiaire[]
 }
 
-const PromotionStagiairesList : React.FC<PromotionStagiairesListProps> = ({promotion, stagiaires}) => {
+const PromotionStagiairesList : React.FC<PromotionStagiairesListProps> = ({promotion}) => {
   
   const [search, setSearch] = useState<string>('')
 
   const [editMode, setEditMode] = useState<boolean>(false)
 
-  const renderPromotionStagiairesList = () => {
+  const [addMode, setAddMode] = useState<boolean>(false)
 
-    const filteredStagiaires = stagiaires.filter((stagiaire: Stagiaire) => {
-      const name = `${stagiaire.first_name.toLocaleLowerCase()} ${stagiaire.last_name.toLocaleLowerCase()} ${stagiaire.email} ${stagiaire.createdAt.toString().slice(0, 10)}`;
-      return search === '' ? stagiaire : name.includes(search);
-    }); 
+  const [validate, setValidate] = useState<boolean>(false)
 
-    return filteredStagiaires.map((stagiaire: Stagiaire) => {
-      return (
-        <PromotionStagiaireListContainer
-          key={stagiaire.id} 
-          promotion={promotion}
-          stagiaire={stagiaire}
-          editMode={editMode}
-        />
-      );
-    });
-    
-  };
+  const [promotionUpdated, setPromotionUpdated] = useState<Promotion>(promotion)
 
   const handleEditMode = () => {
     if (editMode == false) {
@@ -44,29 +32,104 @@ const PromotionStagiairesList : React.FC<PromotionStagiairesListProps> = ({promo
     }
   }
 
+  const handleValidate = () => {
+    setValidate(true)
+  }
+  
+  const handleAddUser = () => {
+    if (addMode == false) {
+      setAddMode(true)
+    } else {
+      setAddMode(false)
+    }
+  }
+
+  const handleDeleteStagiaire = (idStagiaire : number) => {
+    promotionService
+      .deleteStagiairePromotion(promotion.id, idStagiaire)
+      .then(async () => {
+        const promoTest = await promotionService.getPromotionById(promotion.id);
+        console.log(promoTest);
+        return promoTest;
+      })
+      .then((res) => {
+        setPromotionUpdated(res);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const renderPromotionStagiairesList = () => {
+
+    const filteredStagiaires = promotionUpdated.stagiaires.filter((stagiaire: Stagiaire) => {
+      const name = `${stagiaire.first_name.toLocaleLowerCase()} ${stagiaire.last_name.toLocaleLowerCase()} ${stagiaire.email} ${stagiaire.createdAt.toString().slice(0, 10)}`;
+      return search === '' ? stagiaire : name.includes(search);
+    }); 
+
+    return filteredStagiaires.map((stagiaire: Stagiaire) => {
+      return (
+        <PromotionStagiaireListContainer
+          key={stagiaire.id} 
+          promotion={promotionUpdated}
+          stagiaire={stagiaire}
+          editMode={editMode}
+          addMode={addMode}
+          validate={validate}
+          onDeleteStagiaire={handleDeleteStagiaire}
+        />
+      );
+    });
+    
+  };
+
   return (
     <>
-      <section className={'listPromotionFiche'}>
-        <div className="filterBarPromotionFiche">
-          <div className="allPromotionFiche">Stagiaires</div>
-          <input
-            className="searchInputPromotionFiche"
-            type="search"
-            placeholder="   Recherche ..."
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button className='udpateStagaireListButton' onClick={handleEditMode}>
-            <img src={updateLogo} alt="update" className='updatelogo'/>
-          </button>
-          <div className='sumPromotionFiche'>{stagiaires.length}</div>
-        </div>
-        <div className="gridStagiairePromotionFiche">
-          <h3 className="gridTitlePromotionFiche">Nom</h3>
-          <h3 className="gridTitlePromotionFiche">Prénom</h3>
-          <h3 className="gridTitlePromotionFiche">Email</h3>
-        </div>
-        <div className="listContainerPromotionFiche">{renderPromotionStagiairesList()}</div>
-      </section>
+      {editMode ? (
+        <>
+          <section className={'listPromotionFiche'}>
+            <div className="filterBarPromotionFiche">
+              <div className="allPromotionFiche">Stagiaires</div>
+              <input
+                className="searchInputPromotionFiche"
+                type="search"
+                placeholder="   Recherche ..."
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <img src={validateLogo} alt="validate" className='validateLogo' onClick={handleValidate}/>
+              <img src={updateLogo} alt="update" className='updatelogo' onClick={handleEditMode}/>
+              <div className='sumPromotionFiche'>{promotion.stagiaires.length}</div>
+            </div>
+            <div className="gridStagiairePromotionFicheUpdate">
+              <h3 className="gridTitlePromotionFiche">Nom</h3>
+              <h3 className="gridTitlePromotionFiche">Prénom</h3>
+              <h3 className="gridTitlePromotionFiche">Email</h3>
+              <img src={addUserLogo} alt="addStagiaire" className='addLogo' onClick={handleAddUser}/>
+            </div>
+            <div className="listContainerPromotionFiche">{renderPromotionStagiairesList()}</div>
+          </section>
+        </>
+      ) : (
+        <>
+          <section className={'listPromotionFiche'}>
+            <div className="filterBarPromotionFiche">
+              <div className="allPromotionFiche">Stagiaires</div>
+              <input
+                className="searchInputPromotionFiche"
+                type="search"
+                placeholder="   Recherche ..."
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <img src={updateLogo} alt="update" className='updatelogo' onClick={handleEditMode}/>
+              <div className='sumPromotionFiche'>{promotion.stagiaires.length}</div>
+            </div>
+            <div className="gridStagiairePromotionFiche">
+              <h3 className="gridTitlePromotionFiche">Nom</h3>
+              <h3 className="gridTitlePromotionFiche">Prénom</h3>
+              <h3 className="gridTitlePromotionFiche">Email</h3>
+            </div>
+            <div className="listContainerPromotionFiche">{renderPromotionStagiairesList()}</div>
+          </section>
+        </>
+      )}
     </>
   )
 }
