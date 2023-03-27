3ea904bed76e8835b4d840adc457ac62cea12b39
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-27 17:13:40 +0200

    add react-Modal for handleDelete

--- src/components/listContainers/Promotion/PromotionStagiaireListContainer.tsx
+++ src/components/listContainers/Promotion/PromotionStagiaireListContainer.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Stagiaire } from '../../../models/Stagiaire'
 import '../../../assets/styles/components/listContainer/PromotionFicheListContainer.css'
-import { promotionService } from '../../../services/Reservation/PromotionService'
-import { Promotion } from '../../../models/Reservation/Promotion'
+import Modal from 'react-modal'
+import { ModalStyle } from '../../../assets/styles/components/modals/ModalStyle.css'
+import DeleteConfirmation from '../../modals/DeleteConfirmation'
 
 type PromotionStagiaireListContainerProps = {
-    promotion: Promotion
     stagiaire: Stagiaire
     editMode: boolean
     addMode: boolean
@@ -13,15 +13,38 @@ type PromotionStagiaireListContainerProps = {
     onDeleteStagiaire: Function
 }
 
-const PromotionStagiaireListContainer : React.FC<PromotionStagiaireListContainerProps> = ({promotion, stagiaire, editMode, addMode, validate, onDeleteStagiaire}) => {
+const PromotionStagiaireListContainer : React.FC<PromotionStagiaireListContainerProps> = ({stagiaire, editMode, addMode, validate, onDeleteStagiaire}) => {
+    
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
+
+    const handleDelete = () => {
+        setShowDeleteConfirmation(true)
+    };
+    
+    const handleCancelDelete = () => {
+        setShowDeleteConfirmation(false)
+    };
+    
+    
     return (
         <>
+            <Modal
+                isOpen={showDeleteConfirmation}
+                onRequestClose={handleCancelDelete}
+                contentLabel="Confirmation de suppression"
+                style={ModalStyle}
+            >
+                <DeleteConfirmation
+                  onConfirm={() => onDeleteStagiaire(stagiaire.id)}
+                  onCancel={handleCancelDelete} 
+            />
+            </Modal>
             {editMode ? (
                 <div  className='itemsContainerStagiairesPromotion'>
                     <span className="itemsStagiairesPromotion">{stagiaire.last_name}</span>
                     <span className="itemsStagiairesPromotion">{stagiaire.first_name}</span>
                     <span className="items2StagiairesPromotion">{stagiaire.email}</span>
-                    <button onClick={() => onDeleteStagiaire(stagiaire.id)}>X</button>
+                    <button onClick={handleDelete}>X</button>
                 </div>
             ) : (
                 <div  className='itemsContainerStagiairesPromotion'>
