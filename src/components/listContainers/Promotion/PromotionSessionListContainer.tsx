c94ffe7ac6e31c7960cc6607e0f94282a477de69
unknown <clement.thibaut.guillaume@gmail.com>
2023-03-29 17:35:33 +0200

    add Modal for delete and editMode

--- src/components/listContainers/Promotion/PromotionSessionListContainer.tsx
+++ src/components/listContainers/Promotion/PromotionSessionListContainer.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Session } from '../../../models/Reservation/Session'
 import '../../../assets/styles/components/listContainer/PromotionFicheListContainer.css'
+import Modal from 'react-modal'
+import { ModalStyle } from '../../../assets/styles/components/modals/ModalStyle.css'
+import DeleteConfirmation from '../../modals/DeleteConfirmation'
+import removeLogo from '../../../assets/img/remove.png'
 
 type PromotionSessionListContainerProps = {
     session: Session
+    editMode: boolean
+    onDeleteSession: Function
 }
 
-const PromotionSessionListContainer : React.FC<PromotionSessionListContainerProps>= ({session}) => {
+const PromotionSessionListContainer : React.FC<PromotionSessionListContainerProps>= ({session, editMode, onDeleteSession}) => {
+
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
+
+    const handleDelete = () => {
+        setShowDeleteConfirmation(true)
+    };
+    
+    const handleCancelDelete = () => {
+        setShowDeleteConfirmation(false)
+    };
 
     const formateDate = (date : Date) => {
         const formatedDate : string = 
@@ -18,12 +34,35 @@ const PromotionSessionListContainer : React.FC<PromotionSessionListContainerProp
       }
 
   return (
-<div className='itemsContainerSessionsPromotion'>
-    <span className="itemsSessionsPromotion">{session.desc}</span>
-    <span className="itemsSessionsPromotion">{formateDate(session.startAt)}</span>
-    <span className="itemsSessionsPromotion">{formateDate(session.endAt)}</span>
-    <span className="items2SessionsPromotion">{`${session.formateur.first_name} ${session.formateur.last_name}`}</span>
-</div>
+    <>
+        <Modal
+            isOpen={showDeleteConfirmation}
+            onRequestClose={handleCancelDelete}
+            contentLabel="Confirmation de suppression"
+            style={ModalStyle}
+        >
+        <DeleteConfirmation
+        onConfirm={() => {onDeleteSession(session.id); setShowDeleteConfirmation(false)}}
+        onCancel={handleCancelDelete} 
+        />
+        </Modal>
+        {editMode ? (
+            <div className='itemsContainerSessionsPromotion'>
+                <span className="itemsSessionsPromotion">{session.desc}</span>
+                <span className="itemsSessionsPromotion">{formateDate(session.startAt)}</span>
+                <span className="itemsSessionsPromotion">{formateDate(session.endAt)}</span>
+                <span className="items2SessionsPromotion">{`${session.formateur.first_name} ${session.formateur.last_name}`}</span>
+                <img src={removeLogo} onClick={handleDelete} className="removeLogo"/>
+            </div>
+        ) : (
+            <div className='itemsContainerSessionsPromotion'>
+                <span className="itemsSessionsPromotion">{session.desc}</span>
+                <span className="itemsSessionsPromotion">{formateDate(session.startAt)}</span>
+                <span className="itemsSessionsPromotion">{formateDate(session.endAt)}</span>
+                <span className="items2SessionsPromotion">{`${session.formateur.first_name} ${session.formateur.last_name}`}</span>
+            </div>
+        )}
+    </>
   )
 }
 
