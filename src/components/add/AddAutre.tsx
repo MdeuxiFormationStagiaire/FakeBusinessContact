27109469776f82cfd0a609101aae51eb3ebb94f0
unknown <clement.thibaut.guillaume@gmail.com>
2023-04-04 16:53:34 +0200

    modify desc input to text area and add handleTextChange method to handle it

--- src/components/add/AddAutre.tsx
+++ src/components/add/AddAutre.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Autre } from '../../models/Reservation/Autre';
 import { Salle } from '../../models/Salle';
 import { Utilisateur } from '../../models/Utilisateur';
+import '../../assets/styles/components/add/AddAutre.css'
 
 type AddAutreProps = {
   autres : Autre[]
@@ -17,7 +18,7 @@ const AddAutre : React.FC<AddAutreProps> = ({autres, utilisateur, salles, addNew
 
   const navigate = useNavigate();
 
-  const [newSalle, setNewSalle] = useState<Salle>({ id: 0, name: '', capacity: 0, indication: '', floor: 'RDC', createdAt: new Date() })
+  const [newSalle, setNewSalle] = useState<Salle>(salles[0])
 
   const [autre, setAutre] = useState<Autre>({ id: 0, type: 'Entretien', salle: newSalle, desc: '', createdAt: new Date(), startAt: new Date(), endAt: new Date(), utilisateur:  utilisateur});
 
@@ -25,7 +26,7 @@ const AddAutre : React.FC<AddAutreProps> = ({autres, utilisateur, salles, addNew
     setAutre((autre) => ({...autre, createdAt: new Date()}));
   }, []);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     if (name === 'startAt' || name === 'endAt') {
       const date = new Date(value);
@@ -35,7 +36,12 @@ const AddAutre : React.FC<AddAutreProps> = ({autres, utilisateur, salles, addNew
     }
   }
 
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setAutre(prevState => Object.assign({}, prevState, { [name]: value }));
+  }
+
+  const handleRoomChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = event.target.value;
     const selectedName = event.target.name;
     if (selectedName === "salle") {
@@ -74,9 +80,6 @@ const AddAutre : React.FC<AddAutreProps> = ({autres, utilisateur, salles, addNew
     };
 
     if (!IsOverlapping(autre)) {
-      // autreService.createAutre(autreCapitalized)
-      //   .then(() => onUpdateAutre(autreCapitalized))
-      //   .then(() => setAutre({ id: 0, type: 'Entretien', salle: newSalle, desc: '', createdAt: new Date(), startAt: new Date(), endAt: new Date(), utilisateur: utilisateur}))
       addNewAutre(autreCapitalized)
       setAutre({ id: 0, type: 'Entretien', salle: newSalle, desc: '', createdAt: new Date(), startAt: new Date(), endAt: new Date(), utilisateur: utilisateur})
     } else {
@@ -96,15 +99,15 @@ const AddAutre : React.FC<AddAutreProps> = ({autres, utilisateur, salles, addNew
           <button type="submit" className='addFormButtonAutres'>Ajouter</button>
           <button type='button' className='cancelButtonAutres' onClick={() => navigate(-1)}>Annuler</button>
         </div>
-        <section className="addFicheLeftSectionAutres">
-          <div className="inputDivBoxAutres">
-            <div className='inputDivAutres'>
+        <section className='addFicheSectionAutres'>
+          <section className="addFicheLeftSectionAutres">
+            <div className='inputDivLeftAutres'>
               <label className='addInputTitleAutres'>Type :</label>
               <select
                 name="type"
                 value={autre.type}
                 onChange={handleTypeChange}
-                className='typeInputTextAutres'
+                className='typeAddInputAutres'
               >
                 <option value="Réunion">Réunion</option>
                 <option value="Examen">Examen</option>
@@ -113,58 +116,57 @@ const AddAutre : React.FC<AddAutreProps> = ({autres, utilisateur, salles, addNew
                 <option value="Visioconférence">Visioconférence</option>
               </select>
             </div>
-            <div className='inputDivAutres'>
+            <div className='inputDivLeftAutres'>
               <label className='addInputTitleAutres'>DD :</label>
               <input
                 type="date"
                 name="startAt"
                 id="startAt"
-                className='dateInputAutres'
+                className='dateAddInputAutres'
                 value={formateDateForm(autre.startAt)}
-                onChange={handleInputChange}
+                onChange={handleDateChange}
                 required
               />
             </div>
-            <div className='inputDivAutres'>
+            <div className='inputDivLeftAutres'>
               <label className='addInputTitleAutres'>Salle :</label>
               <select
                 name="salle"
                 value={autre.salle.name}
-                onChange={handleSelectChange}
-                className='typeInputTextAutres'
+                onChange={handleRoomChange}
+                className='typeAddInputAutres'
               >
                 {salles.map((salle) => (
                   <option key={salle.id} value={salle.name}>{salle.name}</option>
                 ))}
               </select>
             </div>
-            <div className='inputDivAutres'>
+            <div className='inputDivLeftAutres'>
               <label className='addInputTitleAutres'>DF :</label>
               <input
                 type="date"
                 name="endAt"
                 id="endAt"
-                className='dateInputAutres'
+                className='dateAddInputAutres'
                 value={formateDateForm(autre.endAt)}
-                onChange={handleInputChange}
+                onChange={handleDateChange}
                 required
               />
             </div>
-          </div>
-        </section>
-        <section className='addFicheRightSectionAutres'>
-          <div className='inputDivAutres'>
-            <label htmlFor="first_name" className='addInputTitleAutres'>Description :</label>
-            <input
-              type="text"
-              name="desc"
-              id="desc"
-              className='descInputTextAutres'
-              value={autre.desc}
-              onChange={handleInputChange}
-              required
-            />
-          </div>
+          </section>
+          <section className='addFicheRightSectionAutres'>
+            <div className='inputDivRightAutres'>
+              <label htmlFor="first_name" className='addInputTitleAutres'>Description :</label>
+              <textarea
+                name="desc"
+                id="desc"
+                className='descInputAddTextAutres'
+                value={autre.desc}
+                onChange={handleTextChange}
+                required
+              />
+            </div>
+          </section>
         </section>
       </form>
     </>
