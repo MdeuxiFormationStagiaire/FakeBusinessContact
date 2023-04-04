de49059719ea28c7173089229b9b13ab13fbf89a
unknown <clement.thibaut.guillaume@gmail.com>
2023-04-04 12:39:55 +0200

    setup addPage with addAutre component and AutreList component

--- src/pages/Reservations/Autres/ReservationsAutresAddPage.tsx
+++ src/pages/Reservations/Autres/ReservationsAutresAddPage.tsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Autre } from '../../../models/Reservation/Autre'
+import { autreService } from '../../../services/Reservation/AutreService'
+import AutreList from '../../../components/lists/Autre/AutreList'
+import AddAutre from '../../../components/add/AddAutre'
+import { utilisateurService } from '../../../services/UtilisateurService'
+import { Utilisateur } from '../../../models/Utilisateur'
+import { salleService } from '../../../services/SalleService'
+import { Salle } from '../../../models/Salle'
 
 const ReservationsAutresAddPage = () => {
+
+  const idUtilisateur : number = 11
+
+  const [autres, setAutres] = useState<Autre[]>([])
+
+  const [utilisateur, setUtilisateur] = useState<Utilisateur>()
+
+  const [salles, setSalles] = useState<Salle[]>([])
+
+  const [currentPage, setCurrentPage] = useState('Add')
+
+  
+  useEffect(() => {
+    getAllAutres()
+    getUtilisateurById(idUtilisateur)
+    getAllSalles()
+  }, [])
+  
+  const getAllAutres = () => {
+    autreService.findAllAutres().then(data => setAutres(data))
+  }
+  
+  const getUtilisateurById = (id : number) => {
+    utilisateurService.getUtilisateurById(id).then(data => setUtilisateur(data))
+  }
+  
+  const getAllSalles = () => {
+    salleService.findAllSalles().then(data => setSalles(data))
+  }
+
+  const addNewAutre = (autre : Autre) => {
+    autreService.createAutre(autre).then(() => getAllAutres())
+  }
+  
   return (
-    <div>ReservationsAutresAddPage</div>
+    <>
+    {autres &&
+      <>
+        {utilisateur &&
+          <AddAutre
+            autres={autres}
+            utilisateur={utilisateur}
+            salles={salles}
+            addNewAutre={addNewAutre}
+          />
+        }
+        <AutreList
+          autres={autres} 
+          currentPage={currentPage}
+        />
+      </>
+    }
+    </>
   )
 }
 
