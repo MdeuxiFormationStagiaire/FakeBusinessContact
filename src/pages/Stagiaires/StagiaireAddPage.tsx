ced24a8caf30fba38fcfa5c2678f652ee9d713c2
unknown <clement.thibaut.guillaume@gmail.com>
2023-04-18 17:06:31 +0200

    add addNewStagiairesByImport method for create severals stagiaires via CSV file

--- src/pages/Stagiaires/StagiaireAddPage.tsx
+++ src/pages/Stagiaires/StagiaireAddPage.tsx
@@ -6,7 +6,7 @@ import { stagiaireService } from '../../services/StagiaireService'
 
 const StagiaireAddPage = () => {
   
-  const [stagiaires, setStagiaires] = useState<Stagiaire[]>([])
+  const [stagiairesList, setStagiairesList] = useState<Stagiaire[]>([])
 
   const [currentPage, setCurrentPage] = useState('Add')
 
@@ -15,17 +15,41 @@ const StagiaireAddPage = () => {
   }, [])
 
   const getAllStagiaires = () => {
-    stagiaireService.findAllStagiaires().then(data => setStagiaires(data))
+    stagiaireService.findAllStagiaires().then(data => setStagiairesList(data))
   }
   
   const addNewStagiaire = (stagiaire : Stagiaire) => {
     stagiaireService.createStagiaire(stagiaire).then(() => getAllStagiaires())
   }
 
+  const addNewStagiairesByImport = async (stagiaires : Stagiaire[]) => {
+    const failedStagiaires: {nom: string, prenom: string}[] = [];
+
+    for (const stagiaire of stagiaires) {
+      const existingStagiaire = stagiairesList.find(s => s.email === stagiaire.email);
+  
+      if (existingStagiaire) {
+        failedStagiaires.push({nom: stagiaire.last_name, prenom: stagiaire.first_name});
+      } else {
+        await stagiaireService.createStagiaire(stagiaire);
+      }
+    }
+  
+    if (failedStagiaires.length > 0) {
+      const failedStagiairesMsg = failedStagiaires.map(s => `${s.prenom} ${s.nom}`).join(', ');
+      alert(`Les stagiaires suivants n'ont pas pu être créés : ${failedStagiairesMsg}. Ils existent déjà dans la base de données.`);
+    }
+  
+    getAllStagiaires();
+  }
+
   return (
     <>
-      <AddStagiaire addNewStagiaire={addNewStagiaire}/>
-      <StagiaireList stagiaires={stagiaires} currentPage={currentPage}/>
+      <AddStagiaire 
+        addNewStagiaire={addNewStagiaire}
+        addNewStagiairesByImport={addNewStagiairesByImport}
+      />
+      <StagiaireList stagiaires={stagiairesList} currentPage={currentPage}/>
     </>
   )
 }
