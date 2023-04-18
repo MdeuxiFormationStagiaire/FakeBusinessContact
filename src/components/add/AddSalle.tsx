c2c5dcc8feb2f5b9315485c1e90f4871866f1510
unknown <clement.thibaut.guillaume@gmail.com>
2023-04-18 14:21:03 +0200

    move position of save and cancel button below add form section

--- src/components/add/AddSalle.tsx
+++ src/components/add/AddSalle.tsx
@@ -5,8 +5,8 @@ import '../../assets/styles/components/add/AddSalle.css'
 
 
 type addSalleProps = {
-    addNewSalle : Function
-  }
+  addNewSalle : Function
+}
 
 const AddSalle : React.FC<addSalleProps> = ({addNewSalle}) => {
 
@@ -16,104 +16,104 @@ const AddSalle : React.FC<addSalleProps> = ({addNewSalle}) => {
   const [salle, setSalle] = useState<Salle>({ id: 0, name: '', capacity: 0, indication: '', floor: 'RDC', createdAt: new Date() });
   
   useEffect(() => {
-      setSalle((salle) => ({...salle, createdAt: new Date()}));
+    setSalle((salle) => ({...salle, createdAt: new Date()}));
   }, []);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      setSalle((salle) => ({...salle, [name]: value}));
+    const { name, value } = event.target;
+    setSalle((salle) => ({...salle, [name]: value}));
   };
 
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      const { name, value } = event.target;
-      setSalle((salle) => ({...salle, [name]: value}));
+    const { name, value } = event.target;
+    setSalle((salle) => ({...salle, [name]: value}));
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      
-      const salleCapitalized : Salle = {
-          ...salle,
-          name: salle.name.charAt(0).toUpperCase() + salle.name.toLocaleLowerCase().slice(1),
-          indication: salle.indication.charAt(0).toUpperCase() + salle.indication.toLocaleLowerCase().slice(1)
-      };
-      const response = await fetch(`${URL}`);
-      const data = await response.json();
-      const duplicateSalle = data.find((salle : Salle) => salle.name === salleCapitalized.name);
-      if (duplicateSalle) {
-          alert('Cette salle existe déjà !');
-      } else {
-          addNewSalle(salleCapitalized);
-          const newSalle : Salle = {id: 0, name: '', capacity: 0, indication: '', floor: 'RDC', createdAt: new Date()};
-          setSalle(newSalle);
-      };
+    event.preventDefault();
+    
+    const salleCapitalized : Salle = {
+        ...salle,
+        name: salle.name.charAt(0).toUpperCase() + salle.name.toLocaleLowerCase().slice(1),
+        indication: salle.indication.charAt(0).toUpperCase() + salle.indication.toLocaleLowerCase().slice(1)
+    };
+    const response = await fetch(`${URL}`);
+    const data = await response.json();
+    const duplicateSalle = data.find((salle : Salle) => salle.name === salleCapitalized.name);
+    if (duplicateSalle) {
+        alert('Cette salle existe déjà !');
+    } else {
+        addNewSalle(salleCapitalized);
+        const newSalle : Salle = {id: 0, name: '', capacity: 0, indication: '', floor: 'RDC', createdAt: new Date()};
+        setSalle(newSalle);
+    };
   };
 
   return (
-      <>
-        <form className='addFormSectionSalles' onSubmit={(event) => handleSubmit(event)}>
-          <div className="buttonDivBoxSalles">
-            <button type="submit" className='addFormButtonSalles'>Ajouter</button>
-            <button type='button' className='cancelButtonSalles' onClick={() => navigate(-1)}>Annuler</button>
-          </div>
-          <section className="addFicheSectionSalles">
-            <div className="inputDivBoxSalles">
-              <div className='inputDivSalles'>
-                <label htmlFor="name" className='addInputTitleSalles'>Nom :</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  className='nameAddInputTextSalles'
-                  value={salle.name}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div className='inputDivSalles'>
-                <label htmlFor="capacity" className='addInputTitleSalles'>Capacité :</label>
-                <input
-                  type="number"
-                  name="capacity"
-                  id="capacity"
-                  className='capacityAddInputSalles'
-                  value={salle.capacity}
-                  onChange={handleInputChange}
-                  min="1"
-                  max="99"
-                  required
-                />
-              </div>
-              <div className='inputDivSalles'>
-                <label htmlFor="indication" className='addInputTitleSalles'>Indications :</label>
-                <input
-                  type="text"
-                  name="indication"
-                  id="indication"
-                  className='indicationAddInputTextSalles'
-                  value={salle.indication}
-                  onChange={handleInputChange}
-                  required
-                />
-              </div>
-              <div className='inputDivSalles'>
-                <label htmlFor="floor" className='addInputTitleSalles'>Etage :</label>
-                <select
-                    name="floor"
-                    value={salle.floor}
-                    onChange={handleSelectChange}
-                    className='floorAddInputTextSalles'
-                    required
-                  >
-                    <option value="RDC">RDC</option>
-                    <option value="1er">1er</option>
-                    <option value="2ème">2ème</option>
-                  </select>
-              </div>
+    <>
+      <form className='addFormSectionSalles' onSubmit={(event) => handleSubmit(event)}>
+        <section className="addFicheSectionSalles">
+          <div className="inputDivBoxSalles">
+            <div className='inputDivSalles'>
+              <label htmlFor="name" className='addInputTitleSalles'>Nom :</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className='nameAddInputTextSalles'
+                value={salle.name}
+                onChange={handleInputChange}
+                required
+              />
             </div>
-          </section>
-        </form>
-      </>
+            <div className='inputDivSalles'>
+              <label htmlFor="capacity" className='addInputTitleSalles'>Capacité :</label>
+              <input
+                type="number"
+                name="capacity"
+                id="capacity"
+                className='capacityAddInputSalles'
+                value={salle.capacity}
+                onChange={handleInputChange}
+                min="1"
+                max="99"
+                required
+              />
+            </div>
+            <div className='inputDivSalles'>
+              <label htmlFor="indication" className='addInputTitleSalles'>Indications :</label>
+              <input
+                type="text"
+                name="indication"
+                id="indication"
+                className='indicationAddInputTextSalles'
+                value={salle.indication}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className='inputDivSalles'>
+              <label htmlFor="floor" className='addInputTitleSalles'>Etage :</label>
+              <select
+                  name="floor"
+                  value={salle.floor}
+                  onChange={handleSelectChange}
+                  className='floorAddInputTextSalles'
+                  required
+                >
+                  <option value="RDC">RDC</option>
+                  <option value="1er">1er</option>
+                  <option value="2ème">2ème</option>
+                </select>
+            </div>
+          </div>
+          <div className="buttonDivBoxSalles">
+            <button type='button' className='cancelButtonSalles' onClick={() => navigate(-1)}>Annuler</button>
+            <button type="submit" className='addFormButtonSalles'>Ajouter</button>
+          </div>
+        </section>
+      </form>
+    </>
   );
 }
 
