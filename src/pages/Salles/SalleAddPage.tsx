687bb486806ed8908fa5f660bdbfa1c43046d872
unknown <clement.thibaut.guillaume@gmail.com>
2023-04-18 19:06:46 +0200

    create new method for import CSV of several users and  pass it to children AddSalle

--- src/pages/Salles/SalleAddPage.tsx
+++ src/pages/Salles/SalleAddPage.tsx
@@ -6,7 +6,7 @@ import { salleService } from '../../services/SalleService'
 
 const SalleAddPage = () => {
 
-  const [salles, setSalles] = useState<Salle[]>([])
+  const [sallesList, setSallesList] = useState<Salle[]>([])
 
   const [currentPage, setCurrentPage] = useState('Add')
 
@@ -15,18 +15,41 @@ const SalleAddPage = () => {
   }, [])
 
   const getAllSalles = () => {
-    salleService.findAllSalles().then(data => setSalles(data))
+    salleService.findAllSalles().then(data => setSallesList(data))
   }
   
   const addNewSalle = (salle : Salle) => {
     salleService.createSalle(salle).then(() => getAllSalles())
   }
 
+  const addNewSallesByImport = async (salles : Salle[]) => {
+    const failedSalles: {nom: string}[] = [];
+
+    for (const salle of salles) {
+      const existingSalle = sallesList.find(s => s.name === salle.name);
   
+      if (existingSalle) {
+        failedSalles.push({nom: salle.name});
+      } else {
+        await salleService.createSalle(salle);
+      }
+    }
+  
+    if (failedSalles.length > 0) {
+      const failedSallesMsg = failedSalles.map(s => `${s.nom}`).join(', ');
+      alert(`Les stagiaires suivants n'ont pas pu être créés : ${failedSallesMsg}. Ils existent déjà dans la base de données.`);
+    }
+  
+    getAllSalles();
+  }
+
   return (
     <>
-      <AddSalle addNewSalle={addNewSalle}/>
-      <SalleList salles={salles} currentPage={currentPage}/>
+      <AddSalle 
+        addNewSalle={addNewSalle}
+        addNewSallesByImport={addNewSallesByImport}
+      />
+      <SalleList salles={sallesList} currentPage={currentPage}/>
     </>
   )
 }
