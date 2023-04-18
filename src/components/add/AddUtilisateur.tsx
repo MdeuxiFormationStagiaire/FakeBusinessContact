87ab65282d63eab8ecfb5293821d251afab54871
unknown <clement.thibaut.guillaume@gmail.com>
2023-04-18 19:13:40 +0200

    add new state File, add new method handleFileChange, add new method handleFileSubmit and adapt html for it

--- src/components/add/AddUtilisateur.tsx
+++ src/components/add/AddUtilisateur.tsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Utilisateur } from '../../models/Utilisateur';
 import '../../assets/styles/components/add/AddUtilisateur.css'
+import Papa from 'papaparse';
 
 type AddUtilisateurProps = {
-  addNewUtilisateur : Function
+  addNewUtilisateur : (utilisateur : Utilisateur) => void
+  addNewUtilisateursByImport : (utilisateurs : Utilisateur[]) => void
 }
 
-const AddUtilisateur : React.FC<AddUtilisateurProps> = ({addNewUtilisateur}) => {
+const AddUtilisateur : React.FC<AddUtilisateurProps> = ({addNewUtilisateur, addNewUtilisateursByImport}) => {
 
   const URL = process.env.REACT_APP_DB_USER_URL;
 
   const navigate = useNavigate();
+
   const [utilisateur, setUtilisateur] = useState<Utilisateur>({ id: 0, first_name: '', last_name: '', email: '', position: '', createdAt: new Date(), adminRight: false });
+
+  const [file, setFile] = useState<File | null>(null);
 
   useEffect(() => {
     setUtilisateur((utilisateur) => ({...utilisateur, createdAt: new Date()}));
@@ -27,15 +32,21 @@ const AddUtilisateur : React.FC<AddUtilisateurProps> = ({addNewUtilisateur}) => 
     }
   };
 
+  const handleFileChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  }
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const utilisateurCapitalized : Utilisateur = {
       ...utilisateur,
-      first_name: utilisateur.first_name.charAt(0).toUpperCase() + utilisateur.first_name.toLocaleLowerCase().slice(1),
-      last_name: utilisateur.last_name.charAt(0).toUpperCase() + utilisateur.last_name.toLocaleLowerCase().slice(1),
-      email: utilisateur.email.toLocaleLowerCase(),
-      position: utilisateur.position.charAt(0).toUpperCase() + utilisateur.position.toLocaleLowerCase().slice(1)
+      first_name: utilisateur.first_name.charAt(0).toUpperCase() + utilisateur.first_name.toLowerCase().slice(1),
+      last_name: utilisateur.last_name.toUpperCase(),
+      email: utilisateur.email.toLowerCase(),
+      position: utilisateur.position.charAt(0).toUpperCase() + utilisateur.position.toLowerCase().slice(1)
     };
 
     const response = await fetch(`${URL}`);
@@ -51,78 +62,122 @@ const AddUtilisateur : React.FC<AddUtilisateurProps> = ({addNewUtilisateur}) => 
     };
   };
 
+  const handleFileSubmit = (event : React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        complete : (results) => {
+          const utilisateurs : Utilisateur[] = results.data.map((data : any) => ({
+            id: 0,
+            first_name: data.first_name.charAt(0).toUpperCase() + data.first_name.toLowerCase().slice(1),
+            last_name: data.last_name.toUpperCase(),
+            email: data.email.toLowerCase(),
+            position: data.position.charAt(0).toUpperCase() + data.position.toLowerCase().slice(1),
+            createdAt: new Date(),
+            adminRight: false
+          }));
+          addNewUtilisateursByImport(utilisateurs)
+        },
+        error : (error) => {
+          console.log(error);
+          alert("Le fichier CSV est incorrect : " + error.message)
+        }
+      })
+    }
+  }
+
   return (
     <>
-      <form className='addFormSectionUtilisateurs' onSubmit={(event) => handleSubmit(event)}>
-        <section className="addFicheSectionUtilisateurs">
-          <div className="inputDivBoxUtilisateurs">
-            <div className='inputDivUtilisateurs'>
-              <label htmlFor="last_name" className='addInputTitleUtilisateurs'>Nom :</label>
-              <input
-                type="text"
-                name="last_name"
-                id="last_name"
-                className='lastNameInputUtilisateurs'
-                value={utilisateur.last_name}
-                onChange={handleInputChange}
-                required
-              />
+      <section className='addFormsSectionUtilisateurs'>
+        <form className='addFormSectionUtilisateurs' onSubmit={(event) => handleSubmit(event)}>
+          <section className="addFicheSectionUtilisateurs">
+            <div className="inputDivBoxUtilisateurs">
+              <div className='inputDivUtilisateurs'>
+                <label htmlFor="last_name" className='addInputTitleUtilisateurs'>Nom :</label>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  className='lastNameInputUtilisateurs'
+                  value={utilisateur.last_name}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className='inputDivUtilisateurs'>
+                <label htmlFor="first_name" className='addInputTitleUtilisateurs'>Prénom :</label>
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  className='firstNameInputUtilisateurs'
+                  value={utilisateur.first_name}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className='inputDivUtilisateurs'>
+                <label htmlFor="email" className='addInputTitleUtilisateurs'>Email :</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className='emailInputUtilisateurs'
+                  value={utilisateur.email}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className='inputDivUtilisateurs'>
+                <label htmlFor="position" className='addInputTitleUtilisateurs'>Fonction :</label>
+                <input
+                  type="text"
+                  name="position"
+                  id="position"
+                  className='positionInputUtilisateurs'
+                  value={utilisateur.position}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className='inputDivUtilisateurs'>
+                <label htmlFor="adminRight" className='addInputTitleUtilisateurs'>Admin :</label>
+                <input
+                  type="checkbox"
+                  name="adminRight"
+                  id="adminRight"
+                  className='adminRightInputUtilisateurs'
+                  checked={utilisateur.adminRight}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
             </div>
-            <div className='inputDivUtilisateurs'>
-              <label htmlFor="first_name" className='addInputTitleUtilisateurs'>Prénom :</label>
-              <input
-                type="text"
-                name="first_name"
-                id="first_name"
-                className='firstNameInputUtilisateurs'
-                value={utilisateur.first_name}
-                onChange={handleInputChange}
-                required
-              />
+            <div className="buttonDivBoxUtilisateurs">
+              <button type='button' className='cancelButtonUtilisateurs' onClick={() => navigate(-1)}>Annuler</button>
+              <button type="submit" className='addFormButtonUtilisateurs'>Ajouter</button>
             </div>
-            <div className='inputDivUtilisateurs'>
-              <label htmlFor="email" className='addInputTitleUtilisateurs'>Email :</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className='emailInputUtilisateurs'
-                value={utilisateur.email}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <div className='inputDivUtilisateurs'>
-              <label htmlFor="position" className='addInputTitleUtilisateurs'>Fonction :</label>
-              <input
-                type="text"
-                name="position"
-                id="position"
-                className='positionInputUtilisateurs'
-                value={utilisateur.position}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-            <div className='inputDivUtilisateurs'>
-              <label htmlFor="adminRight" className='addInputTitleUtilisateurs'>Admin :</label>
-              <input
-                type="checkbox"
-                name="adminRight"
-                id="adminRight"
-                className='adminRightInputUtilisateurs'
-                checked={utilisateur.adminRight}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
+          </section>
+        </form>
+        <form onSubmit={handleFileSubmit} className='addFormCSVFileSectionUtilisateurs'>
+          <div className='inputCSVDivUtilisateurs'>
+            <label htmlFor="file" className='addCSVFileTitleUtilisateurs'>Importation par fichier CSV :</label>
+            <input 
+              type="file" 
+              name="file" 
+              id="file" 
+              onChange={handleFileChange}
+              accept=".csv"
+              className='inputCSVFileUtilisateurs'
+            />
           </div>
-          <div className="buttonDivBoxUtilisateurs">
-            <button type='button' className='cancelButtonUtilisateurs' onClick={() => navigate(-1)}>Annuler</button>
-            <button type="submit" className='addFormButtonUtilisateurs'>Ajouter</button>
+          <div className="buttonFormCSVBoxUtilisateurs">
+            <button type="submit" className='addCSVFormButtonUtilisateurs'>Importer</button>
           </div>
-        </section>
-      </form>
+        </form>
+      </section>
     </>
   )
 }
