f9c2989d6ec1a5c8f363291be3ea02d6fb9bf5c5
unknown <clement.thibaut.guillaume@gmail.com>
2023-05-09 15:04:34 +0200

    start reservation compoenent system

--- src/pages/Accueil/Home.tsx
+++ src/pages/Accueil/Home.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import HomePlanning from '../../components/Planning/HomePlanning';
+import { Reservation } from '../../models/Reservation/Reservation';
+import { Promotion } from '../../models/Reservation/Promotion';
+import { Autre } from '../../models/Reservation/Autre';
+import { promotionService } from '../../services/Reservation/PromotionService';
+import { autreService } from '../../services/Reservation/AutreService';
 
 const Home = () => {
+
+  const [promotions, setPromotions] = useState<Promotion[]>([])
+
+  const [autres, setAutres] = useState<Autre[]>([])
+
+  const [reservations, setReservations] = useState<Reservation>({promotions, autres})
+
+  useEffect(() => {
+    getAllReservations()
+  }, [])
+
+  const getAllReservations = () => {
+    autreService.findAllAutres()
+      .then((data) => {
+        setAutres(data);
+        setReservations((prevState) => ({
+          ...prevState,
+          autres : data
+        }));
+      });
+    ;
+    promotionService.findAllPromotions()
+      .then((data) => {
+        setPromotions(data)
+        setReservations((prevState) => ({
+          ...prevState,
+          promotions : data
+        }));
+      });
+    ;
+  }
+  
   return (
-    <div className='title'>HomePage</div>
+    <>
+      <HomePlanning
+        // reservations={reservations}
+      />
+    </>
   )
 }
 
