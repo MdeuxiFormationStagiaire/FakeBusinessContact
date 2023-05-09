4831b5f37b27bbef0ec2acc48b2127806e5e60f8
unknown <clement.thibaut.guillaume@gmail.com>
2023-05-09 14:59:49 +0200

    indent

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -13,148 +13,148 @@ import { IconContext } from "react-icons";
 import "../../assets/styles/components/sidebar/Sidebar.css";
 
 const routes = [
-    {
-        path: "/",
-        name: "Planning",
-        icon: <BsCalendar2Week/>
-    },
-    {
-        path: "/reservations",
-        name: "Réservations",
-        icon: <IoSchoolOutline/>
-    },
-    {
-        path: "/formateurs",
-        name: "Formateurs",
-        icon: <GiTeacher/>
-    },
-    {
-        path: "/salles",
-        name: "Salles",
-        icon: <SiGoogleclassroom/>
-    },
-    {
-        path: "/stagiaires",
-        name: "Stagiaires",
-        icon: <ImProfile/>
-    },
-    {
-        path: "/affichage",
-        name: "Affichage",
-        icon: <TbScreenShare/>
-    },
-    {
-        path: "/utilisateurs",
-        name: "Utilisateurs",
-        icon: <FaUsersCog/>
-    },
+  {
+    path: "/home",
+    name: "Planning",
+    icon: <BsCalendar2Week title="Planning"/>
+  },
+  {
+    path: "/reservations",
+    name: "Réservations",
+    icon: <IoSchoolOutline title="Réservations"/>
+  },
+  {
+    path: "/formateurs",
+    name: "Formateurs",
+    icon: <GiTeacher title="Formateurs"/>
+  },
+  {
+    path: "/salles",
+    name: "Salles",
+    icon: <SiGoogleclassroom title="Salles"/>
+  },
+  {
+    path: "/stagiaires",
+    name: "Stagiaires",
+    icon: <ImProfile title="Stagiaires"/>
+  },
+  {
+    path: "/affichage",
+    name: "Affichage",
+    icon: <TbScreenShare title="Affichage"/>
+  },
+  {
+    path: "/utilisateurs",
+    name: "Utilisateurs",
+    icon: <FaUsersCog title="Utilisateurs"/>
+  },
 ]
 
 const SideBar = ({children} : any) => {
-    const [isOpen, setIsOpen] = useState(false)
 
-    const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false)
 
-    const location = useLocation();
+  const toggle = () => setIsOpen(!isOpen);
 
-    const showAnimation = {
-        hidden: {
-            width: 0,
-            opacity : 0,
-            transition: {
-                duration: 0.5,
-            }
-        },
-        show: {
-            width: "auto",
-            opacity: 1,
-            transition: {
-                duration: 0.2,
-            }
-        }
+  const location = useLocation();
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity : 0,
+      transition: {
+        duration: 0.5,
+      }
+    },
+    show: {
+      width: "auto",
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+      }
     }
-
-    return (
-        <IconContext.Provider
-            value={{ size: '25px'}}
-        >
-        <div className="main-container">
-            <motion.div animate={{width: isOpen ? "200px" : "45px"}} className='sidebar'>
-                <div className="top_section">
-                    <div className="bars">
-                        <FaBars onClick={toggle}/>
-                    </div>
-                    {isOpen && 
-                    <motion.h1 variants={showAnimation}
-                            initial="hidden"
-                            animate="show"
-                            exit="hidden"
-                            className="logo">Dashboard
-                    </motion.h1>}
-                </div>
-                <section className="routes">
-                    <>
-                        {routes.map((route) => {
-                            let className = 'link';
-                            switch (route.name) {
-                              case 'Planning':
-                                className += '-home';
-                                break;
-                              case 'Réservations':
-                                className += '-home';
-                                break;
-                              case 'Formateurs':
-                                className += '-formateurs';
-                                break;
-                              case 'Salles':
-                                className += '-salles';
-                                break;
-                              case 'Stagiaires':
-                                className += '-stagiaires';
-                                break;
-                              case 'Affichage':
-                                className += '-home';
-                                break;
-                              case 'Utilisateurs':
-                                className += '-utilisateurs';
-                                break;
-                              default:
-                                break;
-                            }
-                            const isActive = location.pathname === route.path;
-                            return (
-                              <NavLink to={route.path} className={`${className}${isActive ? '-active' : ''}`}>
-                                <div className="icon">{route.icon}</div>
-                                <AnimatePresence>
-                                  {isOpen && (
-                                    <motion.div
-                                      variants={showAnimation}
-                                      initial="hidden"
-                                      animate="show"
-                                      exit="hidden"
-                                      className="link_text"
-                                    >
-                                      {route.name}
-                                    </motion.div>
-                                  )}
-                                </AnimatePresence>
-                              </NavLink>
-                            );
-                        })}
-                    </>
-                </section>
-                <div className="utilisateur">
-                    <div className="logout"><FiLogOut/></div>
-                    <div className="currentUser">
-                        <h3>Nara Tanier</h3>
-                        <p>Administateur</p>
-                    </div>
-                </div>
-            </motion.div>
-            <main>{children}</main>
+  }
+  return (
+    <IconContext.Provider
+        value={{ size: '25px'}}
+    >
+    <div className="main-container">
+      <motion.div animate={{width: isOpen ? "200px" : "45px"}} className='sidebar'>
+        <div className="top_section">
+          <div className="bars">
+            <FaBars onClick={toggle}/>
+          </div>
+          {isOpen && 
+          <motion.h1 variants={showAnimation}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="logo">Dashboard
+          </motion.h1>}
         </div>
-        </IconContext.Provider>
-    )
+        <section className="routes">
+          <>
+            {routes.map((route) => {
+              let className = 'link';
+              switch (route.name) {
+                case 'Planning':
+                  className += '-home';
+                break;
+                case 'Réservations':
+                  className += '-home';
+                break;
+                case 'Formateurs':
+                  className += '-formateurs';
+                break;
+                case 'Salles':
+                  className += '-salles';
+                break;
+                case 'Stagiaires':
+                  className += '-stagiaires';
+                break;
+                case 'Affichage':
+                  className += '-home';
+                break;
+                case 'Utilisateurs':
+                  className += '-utilisateurs';
+                break;
+                default:
+                break;
+              }
+              const isActive = location.pathname.startsWith(route.path);
+              return (
+                <NavLink to={route.path} className={`${className}${isActive ? '-active' : ''}`}>
+                  <div className="icon">{route.icon}</div>
+                  <AnimatePresence>
+                    {isOpen && (
+                      <motion.div
+                        variants={showAnimation}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="link_text"
+                      >
+                        {route.name}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </NavLink>
+              );
+            })}
+          </>
+        </section>
+        <div className="utilisateur">
+          <div className="logout"><FiLogOut/></div>
+          <div className="currentUser">
+            <h3>Nara Tanier</h3>
+            <p>Administateur</p>
+          </div>
+        </div>
+      </motion.div>
+      <main>{children}</main>
+    </div>
+    </IconContext.Provider>
+  )
 }
 
 export default SideBar;
